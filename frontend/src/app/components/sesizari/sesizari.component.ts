cf8bcf2b69ab69fe4275e4ceea3774746aa5f1d2
Catalina Chirtes <adela.chirtes@gmail.com>
2025-05-31 19:54:54 +0300

    [FE] Sesizari

--- frontend/src/app/components/sesizari/sesizari.component.ts
+++ frontend/src/app/components/sesizari/sesizari.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { environment } from '../../../environments/environments';
+import { MatCardModule } from '@angular/material/card';
 
 
 @Component({
   selector: 'app-sesizari',
-  imports: [RouterModule,],
+  imports: [RouterModule, HttpClientModule, MatCardModule],
   templateUrl: './sesizari.component.html',
   styleUrl: './sesizari.component.scss'
 })
-export class SesizariComponent {
+export class SesizariComponent implements OnInit {
+  data: any[] = [];
+  
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit() {
+    const userId = '69';
+    this.http
+    .get<any[]>(`${environment.baseApiUrl}get_sesizari/user_id/${userId}`)
+          .subscribe((response) => {
+            this.data = response;
+            console.log(response);
+    });
 
+  }
 }
