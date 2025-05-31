d41b4a8a499b2ea1d3b18c24f2045b4aaea86957
George Fofiu <gfofiu@valeo-it.com>
2025-05-31 20:24:53 +0300

    made changes to the sesizari

--- frontend/src/app/components/sesizari/sesizari.component.ts
+++ frontend/src/app/components/sesizari/sesizari.component.ts
@@ -3,27 +3,39 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { environment } from '../../../environments/environments';
 import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-sesizari',
-  imports: [RouterModule, HttpClientModule, MatCardModule],
+  standalone: true,
+  imports: [
+    RouterModule,
+    HttpClientModule,
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatChipsModule
+  ],
   templateUrl: './sesizari.component.html',
-  styleUrl: './sesizari.component.scss'
+  styleUrls: ['./sesizari.component.scss']
 })
 export class SesizariComponent implements OnInit {
   data: any[] = [];
-  
+
   constructor(private http: HttpClient) {}
-  
+
   ngOnInit() {
     const userId = '69';
     this.http
-    .get<any[]>(`${environment.baseApiUrl}get_sesizari/user_id/${userId}`)
-          .subscribe((response) => {
-            this.data = response;
-            console.log(response);
-    });
-
+      .get<any[]>(`${environment.baseApiUrl}get_sesizari/user_id/${userId}`)
+      .subscribe((response) => {
+        this.data = response;
+        console.log('Fetched sesizari:', response);
+      });
   }
 }
