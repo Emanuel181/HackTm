8868ab28b9c00efaeec4796ce2466f7d10a6422f
George Fofiu <gfofiu@valeo-it.com>
2025-05-31 16:26:40 +0300

    removed sesizarile mele

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SesizariComponent } from './components/sesizari/sesizari.component';
-import { SesizarileMeleComponent } from './components/sesizarile-mele/sesizarile-mele.component';
 import { ZoneVizualeComponent } from './components/zone-vizuale/zone-vizuale.component';
 import { HomeComponent } from './components/home/home.component';
 import { MapHeat3DComponent } from './map-heat3-d/map-heat3-d';
@@ -19,10 +18,6 @@ export const routes: Routes = [
   {
     path: 'sesizari',
     component: SesizariComponent
-  },
-  {
-    path: 'sesizarile-mele',
-    component: SesizarileMeleComponent
   },
   {
     path: 'zone-vizuale',
